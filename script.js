2574b10e4980a554eaf7f7f0994aab2c945fa792
Mrmibe122 <129469586+Mrmibe122@users.noreply.github.com>
2024-01-16 17:48:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function generatePage() {
-    window.location.href = 'userPage.html';
+    // Generate a random identifier for the user (you can use a more secure method)
+    const userId = generateRandomId();
+
+    // Redirect to the dynamically generated user page
+    window.location.href = `user-data/user_${userId}.html`;
+}
+
+function generateRandomId() {
+    // Generate a random 8-character alphanumeric string
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomId = '';
+    for (let i = 0; i < 8; i++) {
+        randomId += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return randomId;
 }
