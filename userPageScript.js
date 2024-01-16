8216d5e8e340326c6a78d0ae8917470bb6f9993b
Mrmibe122 <129469586+Mrmibe122@users.noreply.github.com>
2024-01-16 17:56:39 -0500

    Delete userPageScript.js

--- userPageScript.js
+++
@@ -1,8 +0,0 @@
-// userPageScript.js
-// Add an event listener for page unload (refresh or close)
-window.addEventListener('beforeunload', function() {
-    // Implement logic to delete user-specific data or handle cleanup tasks
-    alert("Page is being deleted. Bye!");
-    // Redirect back to the homepage
-    window.location.href = "../index.html";
-});
