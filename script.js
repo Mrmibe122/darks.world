70cdcdf15502d720e46a6316c60cbfdf8dac88fc
Mrmibe122 <129469586+Mrmibe122@users.noreply.github.com>
2024-01-16 22:01:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 document.getElementById('generatePageBtn').addEventListener('click', async () => {
   try {
-    const response = await fetch('/api/generatePage');
+    const response = await fetch('/api/generatePage'); // Updated URL
     const data = await response.json();
     const generatedPageUrl = data.url;
     window.location.href = generatedPageUrl;
