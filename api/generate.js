786533723bb68a7f5caa3ef6ca80aa4233dd8934
Mrmibe122 <129469586+Mrmibe122@users.noreply.github.com>
2024-01-16 23:04:51 -0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,13 @@
 module.exports = (req, res) => {
-  // Generate a unique user ID (you may use a library for this)
-  const userID = Math.random().toString(36).substr(2, 9);
-  
-  // Respond with the generated user ID
-  res.json({ userID });
+  // Check if a valid user ID is provided in the request
+  const providedUserID = req.query.userID;
+  if (providedUserID && /^[a-zA-Z0-9]{9}$/.test(providedUserID)) {
+    // If valid, respond with the provided user ID
+    res.json({ userID: providedUserID });
+  } else {
+    // If invalid or not provided, generate a new user ID
+    const newUserID = Math.random().toString(36).substr(2, 9);
+    // Respond with the new user ID
+    res.json({ userID: newUserID });
+  }
 };
