063652cc52e6114e5f32e2e14c8484e98557c1e8
Mrmibe122 <129469586+Mrmibe122@users.noreply.github.com>
2024-02-05 19:46:57 -0500

    Update app3.js

--- app3.js
+++ app3.js
@@ -37,5 +37,5 @@ function animateRaindrop(raindrop, screenHeight) {
 }
 
 function executeCode() {
-    window.location.href = "https://lootdest.com/s?odjM"; // Replace with your desired URL
+    window.location.href = "https://loot-link.com/s?odjM"; // Replace with your desired URL
 }
